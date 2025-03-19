13ed749cbb2f3189f7e3ca4b26a9bfdc951c2282
John Regan <john.m.regan@gmail.com>
2025-03-19 07:23:29 -0500

    Add dot files

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	root: true,
+	parser: '@babel/eslint-parser',
+	extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
+	settings: {
+		jsdoc: {
+			mode: 'typescript',
+		},
+	},
+	rules: {
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{
+				allowedTextDomain: [ 'codeSamplePlugin' ],
+			},
+		],
+	},
+};
